c80a8e81c4889c2bc3273da57f97fdb3c271979a
Rudra Pratap <96486509+dinavod12@users.noreply.github.com>
2025-11-07 09:14:13 +0530

    Update llm_model.py

--- llm_model.py
+++ llm_model.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI,AzureOpenAIEmbeddings
 
 load_dotenv()
 
@@ -10,8 +10,16 @@ llm = AzureChatOpenAI(
     model="gpt-4o-mini",  
     azure_deployment=os.getenv("OPEN_AI_MODEL"), 
     api_key=os.getenv("OPENAI_API_KEY"),
-    temperature=1
+    temperature=0.6
 )
+
+embedding = AzureOpenAIEmbeddings(    
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("OPENAI_API_KEY"),
+    azure_deployment="Kaarthipoc-text-embedding-ada-002",
+    api_version=os.getenv("OPEN_API_VERSION"),
+    )
+
 
 
 #response = llm.invoke("Write a Python function to reverse a string.")
